5030176ded82ea1b6d0e505fd10e6150c09de53b
Gian Carlo Pace <giancarlo.pace@hyland.com>
2024-12-20 17:27:30 +0100

    refactor: extracted the method that generates the song

--- src/main/java/kata/App.java
+++ src/main/java/kata/App.java
@@ -12,7 +12,15 @@ public class App implements Runnable {
 
     @Override
     public void run() {
-        System.out.println("Hello World!");
+        System.out.println(generateSong());
+    }
+
+    // This method extracted is private and this is a smell
+    // for a missing class
+    // I extracted the method with the test still red.
+    // I should find a better way to do it
+    private String generateSong() {
+        return "Hello World!";
     }
 
     public static void main(String[] args) {
