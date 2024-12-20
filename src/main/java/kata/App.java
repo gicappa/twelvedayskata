638b0a7ca9ffda9df6b3647629606f4faa44ffb7
Gian Carlo Pace <giancarlo.pace@hyland.com>
2024-12-20 17:31:06 +0100

    refactor: extracted an object that generates the song

--- src/main/java/kata/App.java
+++ src/main/java/kata/App.java
@@ -7,6 +7,8 @@ package kata;
 public class App implements Runnable {
 
 
+    private final Song song = new Song();
+
     public App() {
     }
 
@@ -15,12 +17,13 @@ public class App implements Runnable {
         System.out.println(generateSong());
     }
 
-    // This method extracted is private and this is a smell
-    // for a missing class
+    // To address the smell I created a class out of the private
+    // method.
+    //
     // I extracted the method with the test still red.
     // I should find a better way to do it
     private String generateSong() {
-        return "Hello World!";
+        return song.generateSong();
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/kata/Song.java
@@ -0,0 +1,15 @@
+package kata;
+
+public class Song {
+
+    public Song() {
+    }
+
+    // This method extracted is private and this is a smell
+    // for a missing class
+    // I extracted the method with the test still red.
+    // I should find a better way to do it
+    String generateSong() {
+        return "Hello World!";
+    }
+}
