e683603df9f53bade7d1bf6e233e81ce69cc7e8e
Gian Carlo Pace <giancarlo.pace@hyland.com>
2024-12-21 21:50:25 +0100

    feat: added second verse.

--- src/main/java/kata/TwelveDaysSong.java
+++ src/main/java/kata/TwelveDaysSong.java
@@ -8,10 +8,19 @@ class TwelveDaysSong implements Song {
     }
 
     public String verse(int verseNumber) {
-        return """
-            On the first day of Christmas
-            My true love gave to me:
-            A partridge in a pear tree.
-            """;
+        if (verseNumber ==1) {
+            return """
+                On the first day of Christmas
+                My true love gave to me:
+                A partridge in a pear tree.
+                """;
+        } else {
+            return """
+                On the second day of Christmas
+                My true love gave to me:
+                Two turtle doves and
+                A partridge in a pear tree.
+                """;
+        }
     }
 }
--- src/test/java/kata/SongTest.java
+++ src/test/java/kata/SongTest.java
@@ -2,20 +2,40 @@ package kata;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SongTest {
 
+    private TwelveDaysSong song;
+
+    @BeforeEach
+    void beforeEach() {
+        song = new TwelveDaysSong();
+    }
+
     @Test
     void it_returns_the_first_verse() {
-        var song = new TwelveDaysSong();
-
         var actual = song.verse(1);
 
         assertThat(actual)
             .isEqualTo("""
                 On the first day of Christmas
                 My true love gave to me:
+                A partridge in a pear tree.
+                """
+            );
+    }
+
+    @Test
+    void it_returns_the_second_verse() {
+        var actual = song.verse(2);
+
+        assertThat(actual)
+            .isEqualTo("""
+                On the second day of Christmas
+                My true love gave to me:
+                Two turtle doves and
                 A partridge in a pear tree.
                 """
             );
