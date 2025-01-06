692d3c74b2942e19b37fbef4ec5a6fbccb597b8b
Gian Carlo Pace <giancarlo.pace@hyland.com>
2025-01-06 23:52:01 +0100

    feat: song first verse done

---
+++ src/main/java/kata/TwelveDaysSong.java
@@ -0,0 +1,12 @@
+package kata;
+
+public class TwelveDaysSong implements Song {
+
+    @Override
+    public String lyrics() {
+        return """
+                On the first day of Christmas
+                My true love gave to me:
+                A partridge in a pear tree.""";
+    }
+}
---
+++ src/test/java/kata/TwelveDaysSongTest.java
@@ -0,0 +1,18 @@
+package kata;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class TwelveDaysSongTest {
+
+    @Test
+    void it_returns_the_first_verse() {
+        var song = new TwelveDaysSong();
+
+        var actual = song.lyrics();
+
+        assertThat(actual).isEqualTo("On the first day of Christmas\nMy true love gave to me:\nA partridge in a pear tree.");
+    }
+
+}
