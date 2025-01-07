9100a2e66a544d5c09b3cabc910e41e9c1106b25
Gian Carlo Pace <giancarlo.pace@hyland.com>
2025-01-07 21:45:15 +0100

    feat: tested second verse printed

--- src/test/java/kata/TwelveDaysSongTest.java
+++ src/test/java/kata/TwelveDaysSongTest.java
@@ -15,4 +15,17 @@ class TwelveDaysSongTest {
         assertThat(actual).isEqualTo("On the first day of Christmas\nMy true love gave to me:\nA partridge in a pear tree.");
     }
 
+    @Test
+    void it_returns_the_second_verse() {
+        var song = new TwelveDaysSong();
+
+        var actual = song.lyrics(2);
+
+        assertThat(actual).isEqualTo("""
+                On the second day of Christmas
+                My true love gave to me:
+                Two turtle doves and
+                A partridge in a pear tree.""");
+    }
+
 }
