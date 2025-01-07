6a0dd1f55eaabb9d2889d97bac6bfe5738866e8f
Gian Carlo Pace <giancarlo.pace@hyland.com>
2025-01-07 21:55:30 +0100

    feat: tested the third verse

--- src/test/java/kata/TwelveDaysSongTest.java
+++ src/test/java/kata/TwelveDaysSongTest.java
@@ -2,30 +2,48 @@ package kata;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TwelveDaysSongTest {
 
+    private TwelveDaysSong song;
+
+    @BeforeEach
+    void beforeEach() {
+        song = new TwelveDaysSong();
+    }
+
     @Test
     void it_returns_the_first_verse() {
-        var song = new TwelveDaysSong();
-
         var actual = song.lyrics(1);
 
-        assertThat(actual).isEqualTo("On the first day of Christmas\nMy true love gave to me:\nA partridge in a pear tree.");
+        assertThat(actual).isEqualTo(
+            "On the first day of Christmas\nMy true love gave to me:\nA partridge in a pear tree.");
     }
 
     @Test
     void it_returns_the_second_verse() {
-        var song = new TwelveDaysSong();
-
         var actual = song.lyrics(2);
 
         assertThat(actual).isEqualTo("""
-                On the second day of Christmas
-                My true love gave to me:
-                Two turtle doves and
-                A partridge in a pear tree.""");
+            On the second day of Christmas
+            My true love gave to me:
+            Two turtle doves and
+            A partridge in a pear tree.""");
+    }
+
+    @Test
+    void it_returns_the_third_verse() {
+        var actual = song.lyrics(3);
+
+        assertThat(actual).isEqualTo("""
+            On the third day of Christmas
+            My true love gave to me:
+            Three french hens
+            Two turtle doves and
+            A partridge in a pear tree.
+            """);
     }
 
 }
