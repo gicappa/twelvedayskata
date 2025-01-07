ad1a24a0b40e7ab281ca3bf0e5cbb7d4e138329a
Gian Carlo Pace <giancarlo.pace@hyland.com>
2025-01-07 21:46:25 +0100

    feat: printed first and second verse

--- src/main/java/kata/TwelveDaysSong.java
+++ src/main/java/kata/TwelveDaysSong.java
@@ -9,6 +9,14 @@ public class TwelveDaysSong implements Song {
 
     @Override
     public String lyrics(int verseNum) {
+        if (verseNum == 2) {
+            return """
+                    On the second day of Christmas
+                    My true love gave to me:
+                    Two turtle doves and
+                    A partridge in a pear tree.""";
+        }
+
         return """
                 On the first day of Christmas
                 My true love gave to me:
